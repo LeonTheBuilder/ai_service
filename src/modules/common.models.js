e0191a2220ae4ca4cb41eb0bee4cef1ccca65656
chence <leoooon@foxmail.com>
2025-08-07 15:10:16 +0800

    cc

--- src/modules/common.models.js
+++ src/modules/common.models.js
@@ -1,42 +1,8 @@
-const AppCodeMessages = Object.freeze({
+const AiCodeMessages = Object.freeze({
     taskRateLimit: {code: "taskRateLimit", message: 'taskRateLimit'},
 });
 
 
-const RequestTypes = Object.freeze({
-    downloadUrl: 'downloadUrl',
-    ingestSite: 'ingestSite'
-});
-
-
-const UserEventTypes = Object.freeze({
-    siteCreated: 'siteCreated',
-    //
-    siteDownloadInit: 'siteDownloadInit',
-    siteDownloadSuccess: 'siteDownloadSuccess',
-    siteDownloadFail: 'siteDownloadFail',
-    pageDownloading: 'pageDownloading',
-    pageDownloaded: 'pageDownloaded',
-    //
-    extractSinglePageContents: 'extractSinglePageContents',
-    extractCommonContentTasks: 'extractCommonContentTasks',
-    executeCommonContentTask: 'executeCommonContentTask',
-    extractApiContentTasks: 'extractApiContentTasks',
-    executeApiContentTask: 'executeApiContentTask',
-    //
-    siteIngestSuccess: 'siteIngestSuccess',
-    siteIngestFail: 'siteIngestFail',
-});
-
-
-const AppMessageQueues = Object.freeze({
-    bizRequestQ: 'bizRequestQ',
-});
-
-
 module.exports = {
-    AppCodeMessages,
-    RequestTypes,
-    UserEventTypes,
-    AppMessageQueues,
+    AiCodeMessages,
 };
